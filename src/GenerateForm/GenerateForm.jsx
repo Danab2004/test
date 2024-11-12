6ec45607d08377a2efc4c0f47259d0af9ac86a82
Danab2004 <121355248+Danab2004@users.noreply.github.com>
2024-11-12 17:50:39 +0200

    Update GenerateForm.jsx

--- src/GenerateForm/GenerateForm.jsx
+++ src/GenerateForm/GenerateForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./GenerateForm.css";
+import DropDown from "../Components/DropDown/DropDown";
+
 const DynamicForm = ({ originalSchema }) => {
   const [schema, setSchema] = useState(originalSchema?.scheme);
   const [formData, setFormData] = useState({});
@@ -10,7 +12,6 @@ const DynamicForm = ({ originalSchema }) => {
       type: originalSchema.type,
       form: formData,
     };
-    console.log("Form Submitted:", submissionData);
   };
 
   useEffect(() => {
@@ -18,7 +19,7 @@ const DynamicForm = ({ originalSchema }) => {
     Object.entries(schema).forEach(([key, value]) => {
       initialFormData[key] = value.type === "Boolean" ? false : "";
 
-      if (typeof value === "object" && !Array.isArray(value)) {
+      if (Object.entries(value)[0][1] === "object") {
         Object.entries(value).forEach(([subKey, subValue]) => {
           initialFormData[subKey] = subValue.type === "Boolean" ? false : "";
         });
@@ -28,6 +29,7 @@ const DynamicForm = ({ originalSchema }) => {
     setFormData(initialFormData);
   }, [schema]);
 
+
   const handleChange = (e, field) => {
     const value = field.type === "Boolean" ? e.target.checked : e.target.value;
     setFormData({ ...formData, [field.name]: value });
@@ -36,46 +38,47 @@ const DynamicForm = ({ originalSchema }) => {
   const filterSchema = (config, name) => {
     {
       return config.type === "Enum" ? (
-        <select
-          id={name}
-          value={formData[name] || ""}
-          onChange={(e) => handleChange(e, { name, type: config.type })}
-        >
-          <option value="">--Please choose an option--</option>
-          {config.enumValues.map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
+<DropDown id={name} formDataName={formData[name] || ""} onChange={(e) => handleChange(e, { name, type: config.type })}options={config.enumValues} />
+   
       ) : config.type === "String" ? (
-        <input
-          type="text"
-          id={name}
-          value={formData[name] || ""}
-          onChange={(e) => handleChange(e, { name, type: config.type })}
-        />
+        <div className="field">
+          <input
+            type="text"
+            id={name}
+            value={formData[name] || ""}
+            onChange={(e) => handleChange(e, { name, type: config.type })}
+          />
+        </div>
+     
+          
       ) : config.type === "number" ? (
-        <input
-          type="number"
-          id={name}
-          value={formData[name] || ""}
-          onChange={(e) => handleChange(e, { name, type: config.type })}
-        />
+        <div className="field">
+          <input
+            type="number"
+            id={name}
+            value={formData[name] || ""}
+            onChange={(e) => handleChange(e, { name, type: config.type })}
+          />
+        </div>
+        
       ) : config.type === "Boolean" ? (
+        <div className="field">
         <input
           type="checkbox"
           id={name}
           checked={formData[name] || false}
           onChange={(e) => handleChange(e, { name, type: config.type })}
         />
+        </div>
       ) : config.type === "Date" ? (
+        <div className="field">
         <input
           type="date"
           id={name}
           checked={formData[name] || false}
           onChange={(e) => handleChange(e, { name, type: config.type })}
         />
+        </div>
       ) : null;
     }
   };
@@ -84,11 +87,11 @@ const DynamicForm = ({ originalSchema }) => {
       {Object.entries(schema).flatMap(([name, config]) => {
         const isNestedObject = typeof Object.entries(config)[0][1] === "object";
         return (
-          <div key={name}>
+          <div  className="field" key={name}>
             <label htmlFor={name}>{name}</label>
             {isNestedObject
               ? Object.entries(config).map(([innerKey, innerConfig]) => (
-                  <div key={innerKey}>
+                  <div className="fieldInner" key={innerKey}>
                     <label htmlFor={innerKey}>
                       {innerConfig.title || innerKey}
                     </label>
